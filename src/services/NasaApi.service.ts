95a1fb2b8d8e5887a17a2e5c23c8e30f8616c44a
Sylwia Grzegolec <s.grzegolec@gmail.com>
2018-08-01 20:45:38 +0200

    Fixed neos get request url

--- src/services/NasaApi.service.ts
+++ src/services/NasaApi.service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export class NasaApiService {
 	static getNeosData = (startDate: string, stopDate: string) => {
-		return axios.get(`/api/neos/${startDate}/${stopDate}`).then((data) => {
+		return axios.get(`/nasa/neos/${startDate}/${stopDate}`).then((data) => {
 			return data.data.near_earth_objects;
 		}).catch((error) => {
 			console.log('ERROR: ', error);
